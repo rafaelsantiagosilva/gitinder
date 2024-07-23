6aba4532da186799c2054b84ca153bdb1715febd
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-23 15:13:43 -0300

    Atualizando para dados aleatórios

--- src/api/fetchUser.ts
+++ src/api/fetchUser.ts
@@ -2,13 +2,15 @@ import axios from 'axios';
 import GithubUser from '../interfaces/GithubUser';
 
 export default async function fetchUser(): Promise<GithubUser | undefined> {
-  //   const USER_QUANTITY = 0;
-  //   const API_URL = `https://api.github.com/users?per_page=${USER_QUANTITY}`;
+  const USER_QUANTITY = 10;
+  const API_URL = `https://api.github.com/users?per_page=${USER_QUANTITY}`;
 
   try {
-    const username = 'rafaelsantiagosilva';
-    const user = await axios.get(`https://api.github.com/users/${username}`);
-    return user.data;
+    const response = await axios.get(API_URL);
+    const users = response.data as GithubUser[];
+    const randomUser = users[Math.round(Math.random() * users.length)];
+    const userResponse = await axios.get(`https://api.github.com/users/${randomUser.login}`);
+    return userResponse.data;
   } catch (error) {
     console.error(`Error: Impossible to fetch user. ${error}`);
     return undefined;
