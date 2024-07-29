f79694e0ab5f4679095e80c2bf70edc052a157a6
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-29 20:22:50 -0300

    Adicionando prop-types e tentando trabalhar com absolute paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,4 +15,12 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json'
+      }
+    }
+  },
 }
