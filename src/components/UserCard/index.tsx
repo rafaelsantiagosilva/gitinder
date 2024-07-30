d30300814b9592b7dbb27d2f929ccf9b4d8c0674
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-29 22:26:09 -0300

    Adicionando a funcionalidade de alterar o usuário ao apertar de botões

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,76 +1,123 @@
 import { motion } from 'framer-motion';
+import { BiSolidMessageError } from 'react-icons/bi';
+import { ImSpinner9 } from 'react-icons/im';
 import { useEffect, useState } from 'react';
-import { FaLocationDot } from 'react-icons/fa6';
-import { MdErrorOutline } from 'react-icons/md';
+
 import fetchUser from '../../api/fetchUser';
 import AnimationToSide from '../../interfaces/AnimationToSide';
 import GithubUser from '../../interfaces/GithubUser';
+import AnimationButton from './AnimationButton';
+import UserBioAndTopLangs from './UserBioAndTopLangs';
 import UserCardHeader from './UserCardHeader';
 import UserFollowingAndFollowers from './UserFollowingAndFollowers';
+import UserLocation from './UserLocation';
 
-interface UserCardProps {
-  isAnimate: boolean;
-  animation: AnimationToSide | undefined;
-}
-
-export default function UserCard({ isAnimate, animation }: UserCardProps) {
+export default function UserCard() {
   const [user, setUser] = useState<GithubUser | undefined>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [animation, setAnimation] = useState<AnimationToSide | undefined>();
+  const [isAnimation, setIsAnimation] = useState(false);
+  const ANIMATION_DURATION = 500; // ms
 
   const getUserFromApi = async () => {
+    setUser(undefined);
+    setIsLoading(true);
     const response = await fetchUser();
     setUser(response);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getUserFromApi();
   }, []);
 
-  /*
-    Animação de ir para direita:
-    - initial={{ x: 0, y: 0, rotate: 0 }} 
-    - animate={{ x: 300, y: 140, rotate: 20 }}
-    - transition={{ duration: 0.5 }}
+  const startAnimation = () => {
+    setIsAnimation(true);
+  };
 
-    Animação de ir para a esquerda:
-    - initial={{ x: 0, y: 0, rotate: 0 }}
-    - animate={{ x: -300, y: 140, rotate: -20 }}
-    - transition={{ duration: 0.5 }}
-  */
+  const endAnimation = () => {
+    setIsAnimation(false);
+  };
+
+  const setAnimationLeft = () => {
+    startAnimation();
+
+    const animationLeft: AnimationToSide = {
+      x: -1000,
+      y: 140,
+      rotate: -15,
+    };
+
+    setAnimation(animationLeft);
+
+    setTimeout(() => {
+      endAnimation();
+    }, ANIMATION_DURATION);
+  };
+
+  const setAnimationRight = () => {
+    startAnimation();
+
+    const animationRight: AnimationToSide = {
+      x: 1000,
+      y: 140,
+      rotate: 15,
+    };
+
+    setAnimation(animationRight);
+
+    setTimeout(() => {
+      endAnimation();
+    }, ANIMATION_DURATION);
+  };
 
   return user ? (
-    <motion.div
-      className="box bg-gray-950 text-gray-200 mx-auto mt-12 w-96 rounded-md shadow-md p-6 transition-transform hover:scale-105"
-      initial={{ x: 0, y: 0, rotate: 0 }}
-      animate={isAnimate ? animation : { x: 0, y: 0, rotate: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <UserCardHeader html_url={user?.html_url} name={user?.name} avatar_url={user?.avatar_url} email={user?.email} />
+    <>
+      <motion.div
+        className="box bg-gray-950 text-gray-200 mx-auto mt-12 w-96 rounded-md shadow-md p-6 transition-transform hover:scale-105"
+        initial={{ x: 0, y: 0, rotate: 0 }}
+        animate={isAnimation ? animation : { x: 0, y: 0, rotate: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <UserCardHeader html_url={user?.html_url} name={user?.name} avatar_url={user?.avatar_url} email={user?.email} />
 
-      <article className="text-sm text-center text-gray-400 my-2 line-clamp-3">
-        <p>{user?.bio}</p>
-      </article>
+        <UserBioAndTopLangs bio={user?.bio} login={user?.login} />
 
-      <figure>
-        <img
-          className="mx-auto"
-          src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user?.login}&layout=compact&bg_color=030712&border_color=E5E7EB&title_color=E5E7EB&text_color=9CA3AF&locale=pt-br&disable_animations`}
-          alt="Top programming languagens of the user"
-        />
-      </figure>
+        <UserFollowingAndFollowers following={user?.following} followers={user?.followers} />
 
-      <UserFollowingAndFollowers following={user?.following} followers={user?.followers} />
+        <UserLocation location={user?.location} />
 
-      {user?.location && (
-        <section className="flex gap-2 text-sm text-gray-600 font-medium justify-start items-center mt-2">
-          <span className="flex items-center gap-1">
-            <FaLocationDot /> {user?.location}
-          </span>
-        </section>
-      )}
-    </motion.div>
+        <div className="flex items-center justify-around pt-4">
+          <AnimationButton
+            animationSide="left"
+            setAnimation={() => {
+              setAnimationLeft();
+
+              setTimeout(() => {
+                getUserFromApi();
+              }, ANIMATION_DURATION);
+            }}
+          />
+
+          <AnimationButton
+            animationSide="right"
+            setAnimation={async () => {
+              setAnimationRight();
+
+              setTimeout(() => {
+                getUserFromApi();
+              }, ANIMATION_DURATION);
+            }}
+          />
+        </div>
+      </motion.div>
+    </>
+  ) : isLoading ? (
+    <ImSpinner9 className="loading size-10 text-gray-400 rotate-180 m-5" />
   ) : (
-    <h2 className="flex justify-center items-center gap-1 text-gray-200 text-center text-md font-medium my-4">
-      <MdErrorOutline /> Quantidade limite de requisições atingida! Por favor, volte mais tarde.
+    <h2 className="text-lg flex items-center text-gray-200 gap-1 font-medium mt-4">
+      Limite de requisições atingido! Volte mais tarde <BiSolidMessageError />
     </h2>
   );
 }
