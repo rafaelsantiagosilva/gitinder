46015e4add7897724d0082e9c8c6a62f5ae744be
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-30 20:04:03 -0300

    Editando o botão de fechar a sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,5 @@
+import { RiCloseLargeFill } from "react-icons/ri";
+
 interface SidebarProps {
   isSidebarOpen: boolean;
   setIsSidebarOpen: (isSidebarOpen: boolean) => void;
@@ -6,11 +8,13 @@ interface SidebarProps {
 export default function Sidebar({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) {
   return (
     <menu
-      className={`bg-gray-800 w-full max-w-96 h-svh fixed top-0 right-0 transition-all ${!isSidebarOpen && 'translate-x-[110%]'}`}
+      className={`bg-gray-800 shadow-sm p-6 w-full max-w-96 h-svh fixed top-0 right-0 transition-all ${!isSidebarOpen && 'translate-x-[110%]'}`}
     >
-      <button onClick={() => setIsSidebarOpen(false)} className="text-lg text-gray-200">
-        X
-      </button>
+      <div className="flex justify-end">
+        <button onClick={() => setIsSidebarOpen(false)} className="pr-4 text-gray-200">
+          <RiCloseLargeFill className="text-2xl flex-1" />
+        </button>
+      </div>
     </menu>
   );
 }
