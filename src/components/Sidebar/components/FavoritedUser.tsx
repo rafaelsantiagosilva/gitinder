c4354cee28531546ab11eaec2f7237af462e92fd
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-31 21:25:16 -0300

    Removendo a tag picture para deixar o tamanho correto na imagem o dev

--- src/components/Sidebar/components/FavoritedUser.tsx
+++ src/components/Sidebar/components/FavoritedUser.tsx
@@ -7,13 +7,11 @@ interface FavoritedUser {
 export default function FavoritedUser({ user }: FavoritedUser) {
   return (
     <a key={user.login} href={user.html_url} target="blank">
-      <section className="flex items-center transition-transform hover:scale-105">
-        <picture>
-          <img src={user.avatar_url} alt="User avatar photo" className="size-16 rounded-full" />
-        </picture>
+      <section className="flex items-center justify-start transition-transform hover:scale-105">
+        <img src={user.avatar_url} alt="User avatar photo" className="w-16 rounded-full" />
         <div className="flex ml-2 flex-col">
           <h3 className="text-gray-200 font-medium">{user?.name || user.login}</h3>
-          <p className="text-gray-400 line-clamp-1">{user?.bio}</p>
+          <p className="text-gray-400 text-xs line-clamp-1">{user?.bio}</p>
         </div>
       </section>
     </a>
