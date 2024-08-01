16fe3eb533008b5cbf17864a716d70da7c422ce9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-31 22:28:27 -0300

    Adicionando o Token de autorização como variável de ambiente

--- src/api/fetchUser.ts
+++ src/api/fetchUser.ts
@@ -2,14 +2,19 @@ import axios from 'axios';
 import GithubUser from '../interfaces/GithubUser';
 
 export default async function fetchUser(): Promise<GithubUser | undefined> {
-  const USER_QUANTITY = 20000;
+  const USER_QUANTITY = 50000;
   const API_URL = `https://api.github.com/users?per_page=${USER_QUANTITY}`;
+
+  const AUTHORIZATION_TOKEN = import.meta.env.VITE_AUTHORIZATION_TOKEN;
+  const API_VERSION = '2022-11-28';
 
   try {
     const response = await axios.get(API_URL);
     const users = response.data as GithubUser[];
     const randomUser = users[Math.round(Math.random() * users.length)];
-    const userResponse = await axios.get(`https://api.github.com/users/${randomUser.login}`);
+    const userResponse = await axios.get(`https://api.github.com/users/${randomUser.login}`, {
+      headers: { Authorization: `Bearer ${AUTHORIZATION_TOKEN}`, 'X-GitHub-Api-Version': API_VERSION },
+    });
     return userResponse.data;
   } catch (error) {
     console.error(`Error: Impossible to fetch user. ${error}`);
